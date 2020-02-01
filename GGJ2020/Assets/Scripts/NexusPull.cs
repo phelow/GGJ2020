40add308179dcaff30fc552e8b825e331f187000
William Pheloung <wpheloung@gmail.com>
2020-01-31 21:12:49 -0800

    Gravity, showing to jenn

---
+++ GGJ2020/Assets/Scripts/NexusPull.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NexusPull : MonoBehaviour
+{
+    internal static NexusPull instance;
+
+    HashSet<Pullable> pullableSpaceObjects = new HashSet<Pullable>();
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(PullObjectsOnPulse());
+    }
+
+    private IEnumerator PullObjectsOnPulse()
+    {
+        while (true)
+        {
+            lock (pullableSpaceObjects)
+            {
+                foreach (Pullable pullable in pullableSpaceObjects)
+                {
+                    pullable.PullTowardsNexus(this.transform.position);
+                }
+            }
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+
+    internal void AddPullableObject(Pullable pullable)
+    {
+        lock (pullableSpaceObjects)
+        {
+            pullableSpaceObjects.Add(pullable);
+
+        }
+    }
+
+    internal void RemovePullableObject(Pullable pullable)
+    {
+        lock (pullableSpaceObjects)
+        {
+            pullableSpaceObjects.Remove(pullable);
+
+        }
+    }
+}
---
+++ GGJ2020/Assets/Scripts/Pullable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A gameobject that is pulled towards the nexus.
+/// </summary>
+public class Pullable : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NexusPull.instance.AddPullableObject(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    internal void PullTowardsNexus(Vector3 nexusPosition)
+    {
+        // Calculate distance to nexus
+        float distance = Vector3.Distance(this.transform.position, nexusPosition);
+
+        const float MaxForce = 10;
+        const float MinForce = 1;
+
+        const float MaxDistance = 100.0f;
+        const float MinDistance = 1.0f;
+
+        const float DistanceExponent = 3.0f;
+        // Calculate force to inflict
+        float force = Mathf.Lerp(MaxForce, MinForce, Mathf.InverseLerp(MinDistance, MaxDistance, Mathf.Pow(distance, DistanceExponent)));
+
+        // Calculate direction to pull object in
+        Vector3 direction = (nexusPosition - this.transform.position).normalized;
+
+        // Multiply force by direction
+        Vector3 multipliedDirection = direction * force;
+
+        // Add force to gameobject
+        rigidbody2D.AddForce(multipliedDirection);
+    }
+}
