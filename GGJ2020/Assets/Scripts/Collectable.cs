aeb22fd1301a977bc5cf0118e9b00ca8d7c8934e
Feddas <Feddas00@gmail.com>
2020-01-31 23:26:04 -0800

    Can click items within a range of an item Collector.

---
+++ GGJ2020/Assets/Scripts/Collectable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// White item is clicked
+/// Green item that is currently hovered over
+/// Yellow items clickable
+/// Red item is not clickable
+/// </summary>
+public class Collectable : MonoBehaviour
+{
+    public enum ClickabilityEnum
+    {
+        Error = 0,
+        OutOfRange,
+        Clickable,
+        Hovering,
+        Clicked,
+    }
+
+    public ClickabilityEnum Clickability;
+
+    public UnityEvent OnClicked;
+
+    //void Start() { }
+    //void Update() { }
+
+    public void ClickItGood()
+    {
+        Debug.Log(this.name + " was clicked");
+        Clickability = ClickabilityEnum.Clicked;
+        if (OnClicked != null)
+        {
+            OnClicked.Invoke();
+        }
+    }
+}
---
+++ GGJ2020/Assets/Scripts/Collector.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Collector : MonoBehaviour
+{
+    [Tooltip("How close the mouse has to be to the collectable for it to be clickable")]
+    public float mouseDistance = .5f;
+
+    [Tooltip("How close this collector has to be to the collectable for it to be clickable")]
+    public float collectionDistance = 2f;
+
+    [Tooltip("Needed to calculate the mouses cursors world position")]
+    public float distanceOfCameraFromGround = 10f;
+
+    public Collectable lastHovering;
+    public Collectable[] allItems;
+
+    private void Awake()
+    {
+        allItems = FindObjectsOfType<Collectable>();
+    }
+
+    // void Start() { }
+
+    void Update()
+    {
+        updateItemClickability();
+
+        if (lastHovering != null && Input.GetButtonUp("Fire1"))
+        {
+            lastHovering.ClickItGood();
+        }
+    }
+
+    void updateItemClickability()
+    {
+        float distanceToPlayer;
+        List<Collectable> inCollectorRange = new List<Collectable>();
+
+        foreach (var collectable in allItems)
+        {
+            distanceToPlayer = 
+                Vector3.Distance(this.transform.position, collectable.transform.position);
+
+            // highlight objects collecatable by player
+            if (distanceToPlayer < collectionDistance
+                // TODO: && check is in Line of Sight
+                )
+            {
+                collectable.Clickability = Collectable.ClickabilityEnum.Clickable;
+                inCollectorRange.Add(collectable);
+            }
+            else
+            {
+                collectable.Clickability = Collectable.ClickabilityEnum.OutOfRange;
+            }
+        }
+
+        if (inCollectorRange == null || inCollectorRange.Count <= 0)
+            return;
+
+        // determine players mouse cursor position in world
+        var mouseInWorld = Input.mousePosition;
+        mouseInWorld.z = distanceOfCameraFromGround;
+        mouseInWorld = Camera.main.ScreenToWorldPoint(mouseInWorld);
+
+        // highlight closest object being hovered over by the mouse
+        var closestToMouse = inCollectorRange
+            .OrderBy(c => Vector3.Distance(c.transform.position, mouseInWorld))
+            .First();
+
+        //Debug.Log(closestToMouse.name + " dist = " + Vector3.Distance(closestToMouse.transform.position, mouseInWorld));
+        if (Vector3.Distance(closestToMouse.transform.position, mouseInWorld) < mouseDistance)
+        {
+            lastHovering = closestToMouse;
+            closestToMouse.Clickability = Collectable.ClickabilityEnum.Hovering;
+        }
+        else
+        {
+            lastHovering = null;
+        }
+    }
+}
