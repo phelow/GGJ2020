f5b35283eb9c1020c017b952aab3b01ac54fd823
William Pheloung <wpheloung@gmail.com>
2020-01-31 22:17:40 -0800

    Advanced missile behaviors

--- GGJ2020/Assets/Scripts/Targetable.cs
+++ GGJ2020/Assets/Scripts/Targetable.cs
@@ -4,4 +4,11 @@ using UnityEngine;
 
 public class Targetable : MonoBehaviour
 {
+    [SerializeField]
+    private Pullable pullable;
+    internal void Hit()
+    {
+        NexusPull.instance.RemovePullableObject(pullable);
+        Destroy(this.gameObject);
+    }
 }
