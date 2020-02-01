d292b986610e710e75760cffe9faa28e76466e7d
Chris <chris.ll.dougherty@gmail.com>
2020-02-01 00:52:00 -0800

    worked on asteroid drift.
    had to use vector 3 since vector 2 is X/Y and not X/Z
    
    sorry william...

--- GGJ2020/Assets/Scripts/AsteroidBeltSpawner.cs
+++ GGJ2020/Assets/Scripts/AsteroidBeltSpawner.cs
@@ -7,22 +7,23 @@ public class AsteroidBeltSpawner : MonoBehaviour
     public int asteroidAmount = 500;
     public int radius = 50;
     public GameObject asteroidObj;
-    Vector3 CenterPos = Vector3.zero;
+    Vector3 CenterPos;
 
     // Start is called before the first frame update
     void Start()
     {
+        CenterPos = transform.position;
         GameObject curAsteroid;
         for (int i = 0; i <= asteroidAmount; i++)
         {
             Vector3 ringVector = new Vector3(Random.Range(-radius, radius), 0, Random.Range(-radius, radius));
             ringVector.Normalize();
+
             //multiply it with a random value to point to a specific location
             // 1st parameter makes it so that asteroids dont spawn in middle.
-            ringVector *= (Random.Range(radius / 1.1f, radius));
-            curAsteroid = GameObject.Instantiate(asteroidObj, CenterPos + ringVector, Quaternion.identity);
+            ringVector *= (Random.Range(radius/1.1f, radius));
+            curAsteroid = GameObject.Instantiate(asteroidObj, CenterPos + ringVector, Quaternion.identity,this.transform);
             curAsteroid.transform.localScale = new Vector3(Random.Range(0.5f, 1), Random.Range(0.5f, 1), Random.Range(0.5f, 1));
-            curAsteroid.transform.parent = this.gameObject.transform;
         }
     }
 }
