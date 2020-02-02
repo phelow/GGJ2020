b84688af4423452ba78c190a6c45d6b4a6accb76
Chris <chris.ll.dougherty@gmail.com>
2020-02-02 05:11:04 -0800

    DJ STALIN IS IN THE BUILDING!

---
+++ GGJ2020/Assets/Scripts/StalinController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalinController : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public float destroyTime = 15;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        destroyTime -= Time.deltaTime;
+
+        if (destroyTime <=0)
+            gameObject.SetActive(false);
+
+    }
+}
