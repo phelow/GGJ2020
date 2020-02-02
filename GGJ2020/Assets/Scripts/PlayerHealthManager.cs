95eb378367f3cc43def8f0dc99ea7c5fe4761297
William Pheloung <wpheloung@gmail.com>
2020-02-01 17:46:42 -0800

    Teleporting player

---
+++ GGJ2020/Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : HealthManager
+{
+    internal override void TakeHit(float damageToTake = 10.0f)
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+}
