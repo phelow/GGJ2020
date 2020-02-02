1c0beab7ed8d4f59573d2b27f21630b2b5216784
William Pheloung <wpheloung@gmail.com>
2020-02-02 13:11:55 -0800

    Bounce

--- GGJ2020/Assets/Scripts/PlayerHealthManager.cs
+++ GGJ2020/Assets/Scripts/PlayerHealthManager.cs
@@ -7,9 +7,19 @@ public class PlayerHealthManager : HealthManager
     [SerializeField]
     private Player player;
 
+    private float bounce = 0.0f;
+
     internal override void TakeHit(Vector2 movementVector, float damageToTake = 10.0f)
     {
+        bounce += 5.0f;
         player.DrainAllJuice();
-        player.MoveInDirection(movementVector);
+        player.MoveInDirection(movementVector * bounce * 200.0f);
+    }
+
+    private void Update()
+    {
+        bounce -= Time.deltaTime;
+
+        bounce = Mathf.Max(0, bounce);
     }
 }
