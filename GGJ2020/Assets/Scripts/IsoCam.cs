1bd19b60f7cce6c4acdc9283cbc86eebae96c61b
ReframeEverything <andrew3a3@gmail.com>
2020-02-01 17:13:31 -0800

    Added iosmetric camera

---
+++ GGJ2020/Assets/Scripts/IsoCam.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class IsoCam : MonoBehaviour {
+    private Camera mCamera;
+
+    #region Constants
+    private const float NORMAL_FOV = 70;
+#endregion
+
+    public float dampTime = 0.5f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+
+    private void Start()
+    {
+        mCamera = GetComponent<Camera>();
+        mCamera.fieldOfView = NORMAL_FOV;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+		if (target) {
+			Vector3 point = Camera.main.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+	}
+}
